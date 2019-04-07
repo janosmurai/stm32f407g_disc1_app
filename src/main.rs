0d54f489905a238736e8c8ebde3ab3e2e1783d77
janosmurai <murai.janos@gmail.com>
2019-04-07 21:43:51 +0200

    LED initialization moved to stm32f407g lib

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,10 @@
 
 extern crate panic_semihosting;
 
-use stm32f4xx_hal::gpio::gpiod::PD12;
-use stm32f4xx_hal::gpio::gpiod::PD13;
-use stm32f4xx_hal::gpio::gpiod::PD14;
-use stm32f4xx_hal::gpio::gpiod::PD15;
-use stm32f4xx_hal::gpio::PushPull;
-use stm32f4xx_hal::gpio::Output;
 use cortex_m;
 use cortex_m_rt::entry;
 use stm32f4xx_hal::{delay::Delay, prelude::*, stm32};
-
-
-struct LEDs
-{
-    green: PD12<Output<PushPull>>,
-    orange: PD13<Output<PushPull>>,
-    red: PD14<Output<PushPull>>,
-    blue: PD15<Output<PushPull>>,
-}
+use stm32f407g_disc1::leds as led_driver;
 
 #[entry]
 fn main() -> !
@@ -32,13 +18,7 @@ fn main() -> !
     let clocks = rcc.cfgr.freeze();
     let mut delay = Delay::new(cortex_peripherals.SYST, clocks);
     
-    let mut leds = LEDs
-    {
-        green: gpiod.pd12.into_push_pull_output(),
-        orange: gpiod.pd13.into_push_pull_output(),
-        red: gpiod.pd14.into_push_pull_output(),
-        blue: gpiod.pd15.into_push_pull_output(),
-    };
+    let mut leds = led_driver::init(gpiod);
 
     leds.orange.set_high();
     delay.delay_ms(200_u16);
